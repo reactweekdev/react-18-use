c37b57c309f9e71b11713fc5da8d85ed5a073e8e
pikqi <perazmn@gmail.com>
2023-03-24 23:45:11 +0100

    Moves router to a seperate file

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,45 +1,7 @@
-import { Suspense } from "react";
-
-import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
-
+import React from "react";
 import "./App.css";
-import UsersPage from "./users/UsersPage";
-import { Audio, Blocks } from "react-loader-spinner";
-import { SpeakersPage } from "./SpeakersPage";
-import Root from "./routes";
-import logo from "../assets/react.svg"
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "/",
-        element: (
-          <Suspense fallback={<p>loading...</p>}>
-            <UsersPage />
-          </Suspense>
-        ),
-      },
-      {
-        path: "/constantine/speakers",
-        element: (
-          <Suspense fallback={<Blocks
-            visible={true}
-            height="80"
-            width="80"
-            ariaLabel="blocks-loading"
-            wrapperStyle={{}}
-            wrapperClass="blocks-wrapper"
-          />}>
-            <SpeakersPage />
-          </Suspense>
-        ),
-      },
-    ],
-  },
-]);
+import logo from "../assets/react.svg";
+import { AppRoutes } from "./AppRoutes";
 
 function App() {
   return (
@@ -47,7 +9,7 @@ function App() {
       <div>
         <img src={logo} className="logo react" alt="React logo" />
       </div>
-      <RouterProvider router={router} />
+      <AppRoutes />
       <p className="read-the-docs">React 18 use() hook showcase</p>
     </div>
   );
